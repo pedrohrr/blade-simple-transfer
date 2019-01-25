13fb6b15f14a3fd72b095e3e3ed9f13cd178a086
pedrohrr <pedro.rib.rod@gmail.com>
2019-01-25 23:26:10 +0100

    AccountCreate validator test

--- src/main/java/com/pedrohrr/simpletransfer/data/account/AccountCreate.java
+++ src/main/java/com/pedrohrr/simpletransfer/data/account/AccountCreate.java
@@ -2,16 +2,18 @@ package com.pedrohrr.simpletransfer.data.account;
 
 import com.pedrohrr.simpletransfer.data.ValidatedData;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Data
 public class AccountCreate extends ValidatedData {
 
     @NotNull
+    @Positive
     private Long client;
     @NotBlank
+    @Length(min = 3, max = 3, message = "currency must be 3 characters")
     private String currency;
     @NotBlank
     private String iban;
---
+++ src/test/java/com/pedrohrr/simpletransfer/data/account/AccountCreateValidatorTest.java
@@ -0,0 +1,52 @@
+package com.pedrohrr.simpletransfer.data.account;
+
+import com.pedrohrr.simpletransfer.data.Validator;
+import com.pedrohrr.simpletransfer.data.client.ClientCreate;
+import com.pedrohrr.simpletransfer.exception.InvalidDataException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class AccountCreateValidatorTest {
+
+    @Rule
+    public ExpectedException expect = ExpectedException.none();
+
+    private Validator validator = new Validator();
+
+    @Test
+    public void validateAllNull() throws InvalidDataException {
+        expect.expect(InvalidDataException.class);
+        expect.expectMessage("currency must not be blank; iban must not be blank; client must not be null");
+
+        AccountCreate a = new AccountCreate();
+        validator.validate(a);
+    }
+
+    @Test
+    public void validateClientNegative() throws InvalidDataException {
+        expect.expect(InvalidDataException.class);
+        expect.expectMessage("client must be greater than 0");
+
+        AccountCreate a = new AccountCreate();
+        a.setClient(-2l);
+        a.setCurrency("USD");
+        a.setIban("000000000");
+        validator.validate(a);
+    }
+
+    @Test
+    public void validateCurrencyWrongLength() throws InvalidDataException {
+        expect.expect(InvalidDataException.class);
+        expect.expectMessage("currency must be 3 characters");
+
+        AccountCreate a = new AccountCreate();
+        a.setClient(4l);
+        a.setCurrency("USDAA");
+        a.setIban("000000000");
+        validator.validate(a);
+    }
+
+}
